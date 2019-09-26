98675d96ceda1b7b34ba8a63f102b6735b111c1f
Whitney Lee <whitneyleephotography@gmail.com>
2019-09-26 07:29:15 -0500

    Complete game logic

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -10,7 +10,9 @@ class Game extends React.Component {
     super(props);
     this.state = {
       grid: Array(13).fill().map(x => Array(13).fill("0")),
-      redIsNext: true
+      redIsNext: true,
+      redCapturedPairs: 0,
+      goldCapturedPairs: 0,
     }
     this.handleClick = this.handleClick.bind(this);
     this.handleReset = this.handleReset.bind(this);
@@ -34,6 +36,9 @@ class Game extends React.Component {
     let newGrid = Array(13).fill().map(x => Array(13).fill('0'));
     this.setState({
       grid: newGrid,
+      redIsNext: true,
+      redCapturedPairs: 0,
+      goldCapturedPairs: 0,
     })
   }
 
@@ -44,8 +49,6 @@ class Game extends React.Component {
         this.setState({
           grid: this.state.grid,
           redIsNext: false,
-          redCapturedPairs: 0,
-          goldCapturedPairs: 0,
         })
       } else {
         this.state.grid[x][y] = 'gold';
@@ -74,12 +77,12 @@ class Game extends React.Component {
 
     if (this.state.redCapturedPairs === 5) {
       setTimeout(()=>{alert(`red wins!`)}, 1);
-      this.handleReset();
+      setTimeout(()=>{this.handleReset()}, 200);
     }
 
     if (this.state.goldCapturedPairs === 5) {
       setTimeout(()=>{alert(`gold wins!`)}, 1);
-      this.handleReset();
+      setTimeout(()=>{this.handleReset()}, 200);
     }
   }
 
@@ -96,7 +99,7 @@ class Game extends React.Component {
           inARow++;
           if (inARow === 5) {
             setTimeout(()=>{alert(`${color} wins!`)}, 1);
-            this.handleReset();
+            setTimeout(()=>{this.handleReset()}, 200);
           }
         } else {
           inARow = 0;
@@ -110,34 +113,37 @@ class Game extends React.Component {
     let color = (this.state.redIsNext === false) ? 'gold' : 'red';
     let opponent = (this.state.redIsNext === true) ? 'gold' : 'red';
 
-    if ((this.state.grid[x - 1][y] === opponent) && (this.state.grid[x - 2][y] === opponent) && (this.state.grid[x - 3][y] === color)) {
-      if (color === 'gold') {
-        this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
-      } else {
-        this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+    if (x - 3 >= 0 && x + 3 <= 12) {
+      if ((this.state.grid[x - 1][y] === opponent) && (this.state.grid[x - 2][y] === opponent) && (this.state.grid[x - 3][y] === color)) {
+        if (color === 'gold') {
+          this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
+        } else {
+          this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+        }
+        this.state.grid[x - 1][y] = "0";
+        this.state.grid[x - 2][y] = "0";
+        this.setState({
+          goldCapturedPairs: this.state.goldCapturedPairs,
+          redCapturedPairs: this.state.redCapturedPairs,
+          grid: this.state.grid
+        }, console.log(this.state))
       }
-      this.state.grid[x - 1][y] = "0";
-      this.state.grid[x - 2][y] = "0";
-      this.setState({
-        goldCapturedPairs: this.state.goldCapturedPairs,
-        redCapturedPairs: this.state.redCapturedPairs,
-        grid: this.state.grid
-      }, console.log(this.state))
-    }
 
-    if ((this.state.grid[x + 1][y] === opponent) && (this.state.grid[x + 2][y] === opponent) && (this.state.grid[x + 3][y] === color)) {
-      if (color === 'gold') {
-        this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
-      } else {
-        this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+
+      if ((this.state.grid[x + 1][y] === opponent) && (this.state.grid[x + 2][y] === opponent) && (this.state.grid[x + 3][y] === color)) {
+        if (color === 'gold') {
+          this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
+        } else {
+          this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+        }
+        this.state.grid[x + 1][y] = "0";
+        this.state.grid[x + 2][y] = "0";
+        this.setState({
+          goldCapturedPairs: this.state.goldCapturedPairs,
+          redCapturedPairs: this.state.redCapturedPairs,
+          grid: this.state.grid
+        }, console.log(this.state))
       }
-      this.state.grid[x + 1][y] = "0";
-      this.state.grid[x + 2][y] = "0";
-      this.setState({
-        goldCapturedPairs: this.state.goldCapturedPairs,
-        redCapturedPairs: this.state.redCapturedPairs,
-        grid: this.state.grid
-      }, console.log(this.state))
     }
   }
 
@@ -154,7 +160,7 @@ class Game extends React.Component {
           inARow++;
           if (inARow === 5) {
             setTimeout(()=>{alert(`${color} wins!`)}, 1);
-            this.handleReset();
+            setTimeout(()=>{this.handleReset()}, 200);
           }
         } else {
           inARow = 0;
@@ -168,34 +174,36 @@ class Game extends React.Component {
     let color = (this.state.redIsNext === false) ? 'gold' : 'red';
     let opponent = (this.state.redIsNext === true) ? 'gold' : 'red';
 
-    if ((this.state.grid[x][y - 1] === opponent) && (this.state.grid[x][y - 2] === opponent) && (this.state.grid[x][y - 3] === color)) {
-      if (color === 'gold') {
-        this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
-      } else {
-        this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+    if (y - 3 >= 0 && y + 3 <= 12) {
+      if ((this.state.grid[x][y - 1] === opponent) && (this.state.grid[x][y - 2] === opponent) && (this.state.grid[x][y - 3] === color)) {
+        if (color === 'gold') {
+          this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
+        } else {
+          this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+        }
+        this.state.grid[x][y - 1] = "0";
+        this.state.grid[x][y - 2] = "0";
+        this.setState({
+          goldCapturedPairs: this.state.goldCapturedPairs,
+          redCapturedPairs: this.state.redCapturedPairs,
+          grid: this.state.grid
+        }, console.log(this.state))
       }
-      this.state.grid[x][y - 1] = "0";
-      this.state.grid[x][y - 2] = "0";
-      this.setState({
-        goldCapturedPairs: this.state.goldCapturedPairs,
-        redCapturedPairs: this.state.redCapturedPairs,
-        grid: this.state.grid
-      }, console.log(this.state))
-    }
 
-    if ((this.state.grid[x][y + 1] === opponent) && (this.state.grid[x][y + 2] === opponent) && (this.state.grid[x][y + 3] === color)) {
-      if (color === 'gold') {
-        this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
-      } else {
-        this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+      if ((this.state.grid[x][y + 1] === opponent) && (this.state.grid[x][y + 2] === opponent) && (this.state.grid[x][y + 3] === color)) {
+        if (color === 'gold') {
+          this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
+        } else {
+          this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+        }
+        this.state.grid[x][y + 1] = "0";
+        this.state.grid[x][y + 2] = "0";
+        this.setState({
+          goldCapturedPairs: this.state.goldCapturedPairs,
+          redCapturedPairs: this.state.redCapturedPairs,
+          grid: this.state.grid
+        }, console.log(this.state))
       }
-      this.state.grid[x][y + 1] = "0";
-      this.state.grid[x][y + 2] = "0";
-      this.setState({
-        goldCapturedPairs: this.state.goldCapturedPairs,
-        redCapturedPairs: this.state.redCapturedPairs,
-        grid: this.state.grid
-      }, console.log(this.state))
     }
   }
 
@@ -216,7 +224,7 @@ class Game extends React.Component {
           inARow++;
           if (inARow === 5) {
             setTimeout(()=>{alert(`${color} wins!`)}, 1);
-            this.handleReset();
+            setTimeout(()=>{this.handleReset()}, 200);
           }
         } else {
           inARow = 0;
@@ -232,34 +240,36 @@ class Game extends React.Component {
     let color = (this.state.redIsNext === false) ? 'gold' : 'red';
     let opponent = (this.state.redIsNext === true) ? 'gold' : 'red';
 
-    if ((this.state.grid[x - 1][y - 1] === opponent) && (this.state.grid[x - 2][y - 2] === opponent) && (this.state.grid[x - 3][y - 3] === color)) {
-      if (color === 'gold') {
-        this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
-      } else {
-        this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+    if (x - 3 >= 0 && x + 3 <= 12 && y - 3 >= 0 && y + 3 <= 12) {
+      if ((this.state.grid[x - 1][y - 1] === opponent) && (this.state.grid[x - 2][y - 2] === opponent) && (this.state.grid[x - 3][y - 3] === color)) {
+        if (color === 'gold') {
+          this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
+        } else {
+          this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+        }
+        this.state.grid[x - 1][y - 1] = "0";
+        this.state.grid[x - 2][y - 2] = "0";
+        this.setState({
+          goldCapturedPairs: this.state.goldCapturedPairs,
+          redCapturedPairs: this.state.redCapturedPairs,
+          grid: this.state.grid
+        }, console.log(this.state))
       }
-      this.state.grid[x - 1][y - 1] = "0";
-      this.state.grid[x - 1][y - 2] = "0";
-      this.setState({
-        goldCapturedPairs: this.state.goldCapturedPairs,
-        redCapturedPairs: this.state.redCapturedPairs,
-        grid: this.state.grid
-      }, console.log(this.state))
-    }
 
-    if ((this.state.grid[x + 1][y + 1] === opponent) && (this.state.grid[x + 2][y + 2] === opponent) && (this.state.grid[x + 3][y + 3] === color)) {
-      if (color === 'gold') {
-        this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
-      } else {
-        this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+      if ((this.state.grid[x + 1][y + 1] === opponent) && (this.state.grid[x + 2][y + 2] === opponent) && (this.state.grid[x + 3][y + 3] === color)) {
+        if (color === 'gold') {
+          this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
+        } else {
+          this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+        }
+        this.state.grid[x + 1][y + 1] = "0";
+        this.state.grid[x + 2][y + 2] = "0";
+        this.setState({
+          goldCapturedPairs: this.state.goldCapturedPairs,
+          redCapturedPairs: this.state.redCapturedPairs,
+          grid: this.state.grid,
+        }, console.log(this.state))
       }
-      this.state.grid[x + 1][y + 1] = "0";
-      this.state.grid[x + 2][y + 2] = "0";
-      this.setState({
-        goldCapturedPairs: this.state.goldCapturedPairs,
-        redCapturedPairs: this.state.redCapturedPairs,
-        grid: this.state.grid
-      }, console.log(this.state))
     }
   }
 
@@ -280,7 +290,7 @@ class Game extends React.Component {
           inARow++;
           if (inARow === 5) {
             setTimeout(()=>{alert(`${color} wins!`)}, 1);
-            this.handleReset();
+            setTimeout(()=>{this.handleReset()}, 200);
           }
         } else {
           inARow = 0;
@@ -295,37 +305,39 @@ class Game extends React.Component {
     let color = (this.state.redIsNext === false) ? 'gold' : 'red';
     let opponent = (this.state.redIsNext === true) ? 'gold' : 'red';
 
-    if ((this.state.grid[x + 1][y - 1] === opponent) && (this.state.grid[x + 2][y - 2] === opponent) && (this.state.grid[x + 3][y - 3] === color)) {
-      if (color === 'gold') {
-        this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
-      } else {
-        this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+    if (x - 3 >= 0 && x + 3 <= 12 && y - 3 >= 0 && y + 3 <= 12) {
+      if ((this.state.grid[x + 1][y - 1] === opponent) && (this.state.grid[x + 2][y - 2] === opponent) && (this.state.grid[x + 3][y - 3] === color)) {
+        if (color === 'gold') {
+          this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
+        } else {
+          this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+        }
+        this.state.grid[x + 1][y - 1] = "0";
+        this.state.grid[x + 2][y - 2] = "0";
+        this.setState({
+          goldCapturedPairs: this.state.goldCapturedPairs,
+          redCapturedPairs: this.state.redCapturedPairs,
+          grid: this.state.grid,
+        }, console.log(this.state))
       }
-      this.state.grid[x + 1][y - 1] = "0";
-      this.state.grid[x + 2][y - 2] = "0";
-      this.setState({
-        goldCapturedPairs: this.state.goldCapturedPairs,
-        redCapturedPairs: this.state.redCapturedPairs,
-        grid: this.state.grid
-      }, console.log(this.state))
-    }
 
-    if ((this.state.grid[x + 1][y - 1] === opponent) && (this.state.grid[x + 2][y - 2] === opponent) && (this.state.grid[x + 3][y - 3] === color)) {
-      if (color === 'gold') {
-        this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
-      } else {
-        this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+      if ((this.state.grid[x - 1][y + 1] === opponent) && (this.state.grid[x - 2][y + 2] === opponent) && (this.state.grid[x - 3][y + 3] === color)) {
+        if (color === 'gold') {
+          this.state.goldCapturedPairs = this.state.goldCapturedPairs + 1;
+        } else {
+          this.state.redCapturedPairs = this.state.redCapturedPairs + 1;
+        }
+        this.state.grid[x - 1][y + 1] = "0";
+        this.state.grid[x - 2][y + 2] = "0";
+        this.setState({
+          goldCapturedPairs: this.state.goldCapturedPairs,
+          redCapturedPairs: this.state.redCapturedPairs,
+          grid: this.state.grid
+        }, console.log(this.state))
       }
-      this.state.grid[x + 1][y - 1] = "0";
-      this.state.grid[x + 2][y - 2] = "0";
-      this.setState({
-        goldCapturedPairs: this.state.goldCapturedPairs,
-        redCapturedPairs: this.state.redCapturedPairs,
-        grid: this.state.grid
-      }, console.log(this.state))
     }
   }
-}
+
 
   render () {
     return (
@@ -335,7 +347,7 @@ class Game extends React.Component {
     </div>
     )
   }
-
+};
 
 
 ReactDOM.render(<Game />, document.getElementById('app'));
