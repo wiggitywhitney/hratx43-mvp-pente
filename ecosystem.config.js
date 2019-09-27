df2c74327ccd227e51e6fe197d957509c1e1a7b5
Whitney Lee <whitneyleephotography@gmail.com>
2019-09-27 13:50:18 -0500

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-13-58-53-201.us-east-2.compute.amazonaws.com',
+      host: 'http://ec2-13-58-53-201.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/FEC.pem',
       ref: 'origin/master',
       repo: 'https://github.com/wiggitywhitney/hratx43-mvp-pente.git',
