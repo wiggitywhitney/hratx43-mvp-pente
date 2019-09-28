da1af309d014a646d673b74857530bf63e71e27d
Whitney Lee <whitneyleephotography@gmail.com>
2019-09-28 14:00:31 -0500

    Setup for new AWS EC2 instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-13-58-53-201.us-east-2.compute.amazonaws.com',
+      host: 'ec2-3-19-76-32.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/FEC.pem',
       ref: 'origin/master',
       repo: 'https://github.com/wiggitywhitney/hratx43-mvp-pente.git',
